5744c4566c3c38105f08926bd7fdb3638de5e238
dominhdang148 <myclone235@gmail.com>
2023-11-04 00:07:11 +0700

    update part 2

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog 1',
+        'content': 'First content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog 2',
+        'content': 'Second content',
+        'date_posted': 'April 2, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts,)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title="About")
 
 
 if __name__ == '__main__':
